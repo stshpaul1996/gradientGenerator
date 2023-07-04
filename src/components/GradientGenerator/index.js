d9249e29386dbd2cbe85a8cdd5969c0b9d2d146f
satish <satish@email>
2023-07-04 03:07:12 +0000

    gradient-generator

---
+++ src/components/GradientGenerator/index.js
@@ -0,0 +1,105 @@
+import {Component} from 'react'
+
+import GradientDirectionItem from '../GradientDirectionItem'
+
+import {
+  GradientGeneratorContainer,
+  ResponsiveContainer,
+  Heading,
+  DirectionsDescription,
+  GradientDirectionList,
+  ColorsPickersDescription,
+  ColorPickerContainer,
+  CustomInputAndColorContainer,
+  ColorValue,
+  CustomInput,
+  GenerateButton,
+} from './styledComponents'
+
+const gradientDirectionsList = [
+  {directionId: 'TOP', value: 'top', displayText: 'Top'},
+  {directionId: 'BOTTOM', value: 'bottom', displayText: 'Bottom'},
+  {directionId: 'RIGHT', value: 'right', displayText: 'Right'},
+  {directionId: 'LEFT', value: 'left', displayText: 'Left'},
+]
+// Write your code here
+class GradientGenerator extends Component {
+  state = {
+    activeGradientDirection: gradientDirectionsList[0].value,
+    fromColorInput: '#8ae323',
+    toColorInput: '#014f7b',
+    gradientValue: `to ${gradientDirectionsList[0].value}, #8ae323, #014f7b`,
+  }
+
+  onGenerate = () => {
+    const {fromColorInput, toColorInput, activeGradientDirection} = this.state
+    this.setState({
+      gradientValue: `to ${activeGradientDirection}, ${fromColorInput}, ${toColorInput}`,
+    })
+  }
+
+  onChangeToColor = event => {
+    this.setState({toColorInput: event.target.value})
+  }
+
+  onChangeFromColor = event => {
+    this.setState({fromColorInput: event.target.value})
+  }
+
+  clickGradientDirectionItem = direction => {
+    this.setState({activeGradientDirection: direction})
+  }
+
+  render() {
+    const {
+      activeGradientDirection,
+      toColorInput,
+      fromColorInput,
+      gradientValue,
+    } = this.state
+
+    return (
+      <GradientGeneratorContainer
+        data-testid="gradientGenerator"
+        gradientValue={gradientValue}
+      >
+        <ResponsiveContainer>
+          <Heading>Generate a CSS Color Gradient</Heading>
+          <DirectionsDescription>Choose Direction</DirectionsDescription>
+          <GradientDirectionList>
+            {gradientDirectionsList.map(each => (
+              <GradientDirectionItem
+                key={each.directionId}
+                gradientDirectionDetails={each}
+                clickGradientDirectionItem={this.clickGradientDirectionItem}
+                isActive={activeGradientDirection === each.value}
+              />
+            ))}
+          </GradientDirectionList>
+          <ColorsPickersDescription>Pick the Colors</ColorsPickersDescription>
+          <ColorPickerContainer>
+            <CustomInputAndColorContainer>
+              <ColorValue>{fromColorInput}</ColorValue>
+              <CustomInput
+                onChange={this.onChangeFromColor}
+                type="color"
+                value={fromColorInput}
+              />
+            </CustomInputAndColorContainer>
+            <CustomInputAndColorContainer>
+              <ColorValue>{toColorInput}</ColorValue>
+              <CustomInput
+                onChange={this.onChangeToColor}
+                type="color"
+                value={toColorInput}
+              />
+            </CustomInputAndColorContainer>
+          </ColorPickerContainer>
+          <GenerateButton onClick={this.onGenerate}>Generate</GenerateButton>
+        </ResponsiveContainer>
+      </GradientGeneratorContainer>
+    )
+  }
+}
+
+export default GradientGenerator
